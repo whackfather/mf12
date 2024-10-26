7fe38d1918ef86b5608a42e3abb6a84850757849
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-10-26 14:30:32 -0500

    Update main.cpp

--- Brakelight/src/main.cpp
+++ Brakelight/src/main.cpp
@@ -9,20 +9,18 @@
 #define LED_COUNT 18
 #define CTX_PIN 4
 #define CRX_PIN 5
-#define FUN_PIN 3
+using namespace std;
 
 // Declare strip object
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Function declarations
 bool braking();
-void rainbow();
 
 // Setup
 void setup() {
   strip.begin();
   strip.show();
-  pinMode(FUN_PIN, INPUT);
 }
 
 // Loop
@@ -31,9 +29,6 @@ void loop() {
     for (int i = 0; i <= LED_COUNT; i++) {
       strip.setPixelColor(i, 255, 0, 0);
     }
-  } 
-  else if (digitalRead(FUN_PIN)) {
-    rainbow();
   } 
   else {
     for (int i = 0; i <= LED_COUNT; i++) {
@@ -47,8 +42,4 @@ void loop() {
 bool braking() {
   // Do some CAN line reading to check if we is braking or not
   return false;
-}
-
-void rainbow() {
-  // Make the lights do a rainbow (wave style? synchronized?)
 }
