6aa87a45f89827cfd242c855e29647e2b8fc42f2
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-07 20:22:26 -0700

    basic setup

--- linearPots/src/main.cpp
+++ linearPots/src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float potValue = analogRead(A0); // Change pin value to reflect testing environment
+  Serial.println(potValue);
+  delay(100);
 }
