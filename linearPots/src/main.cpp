0ca5825e6736e78882dc310ada7e4ff9d489cb97
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-10 10:09:08 -0600

    created linear calibration formulas implemented switch statement to contain all calibration formulas

--- linearPots/src/main.cpp
+++ linearPots/src/main.cpp
@@ -5,7 +5,19 @@ void setup() {
 }
 
 void loop() {
-  float potValue = analogRead(A0); // Change pin value to reflect testing environment
-  Serial.println(potValue);
+  float rawVal = analogRead(A0); // Change pin value to reflect testing environment
+  float compMm = compression(3, rawVal);
+  Serial.println(compMm);
   delay(100);
+}
+
+float compression(int wheel, float rawVal) {
+  switch(wheel) {
+    case 1: // Front pot 1
+      return ((79.0f / 1018.0f) * rawVal) - (79.0f / 1018.0f);
+    case 2: // Front pot 2
+      return ((78.81f / 1020.0f) * rawVal) - (78.81f / 1020.0f);
+    case 3: // Rear pot 1
+      return ((-76.0f / 1023.0f) * rawVal) + 76;
+  }
 }
